69397fad5543fbeb578615d1450eaf3c71bf66a5
tohoangphucdn <tohoangphucdn@yahoo.com>
2020-02-24 19:10:16 -0800

    Modified to work as byte

--- Lab2/Phuc/memory.v
+++ Lab2/Phuc/memory.v
@@ -3,16 +3,14 @@
 
   Input:
     clk		: clock signal
-    addrIn  : 4-bit input address
-    addrOut1: 4-bit output address
-	 addrOut2: 4-bit output address
+    addrIn  : 32-bit input address
+    addrOut : 32-bit output address
     dataIn  : 32-bit input data
     wr      : write signal
-    rd1, rd2: read signals
+    rd1		: read signals
 
   Output:
-    dataOut1: data at the requested address #1
-	 dataOut2: data at the requested address #2
+    dataOut: data at the requested address
 */
 module memory(
 	input wire clk,
@@ -22,7 +20,7 @@ module memory(
 	input wire wr, rd,
 	output wire [31:0] dataOut) ;
 	
-	reg [31:0] mem [31:0];
+	reg [7:0] mem [31:0];
 	reg [31:0] out;
 	
 	integer i;		
@@ -36,9 +34,29 @@ module memory(
 	end
 	
 	always @(posedge clk) begin
-		if (wr)
-			mem[addrIn] <= dataIn;
-		if (rd)
-			out <= mem[addrOut];
+		if (wr) begin
+			mem[addrIn] <= dataIn[7:0];
+			
+			if (addrIn < (~32'b0)) mem[addrIn + 32'b1] <= dataIn[15:8];
+			else mem[addrIn - (~32'b0) + 32'b1] <= dataIn[15:8];
+			
+			if (addrIn + 32'b1 < (~32'b0)) mem[addrIn + 32'b10] <= dataIn[23:16];
+			else mem[addrIn - (~32'b0) + 32'b10] <= dataIn[23:16]; 
+			
+			if (addrIn + 32'b10 < (~32'b0)) mem[addrIn + 32'b11] <= dataIn[31:24];
+			else mem[addrIn - (~32'b0) + 32'b11] <= dataIn[31:24];
+		end
+		if (rd) begin
+			out[7:0] <= mem[addrOut];
+			
+			if (addrOut < (~32'b0)) out[15:8] <= mem[addrOut + 32'b1];
+			else out[15:8] <= mem[addrOut - (~32'b0) + 32'b1];
+			
+			if (addrOut + 32'b1 < (~32'b0)) out[15:8] <= mem[addrOut + 32'b1];
+			else out[23:16] <= mem[addrOut - (~32'b0) + 32'b10];
+			
+			if (addrOut + 32'b10 < (~32'b0)) out[15:8] <= mem[addrOut + 32'b1];
+			else out[31:24] <= mem[addrOut - (~32'b0) + 32'b11];
+		end
 	end
 endmodule
