41bf0335a5c8d71a988b2f1c1870c2e872dfd955
tohoangphucdn <tohoangphucdn@yahoo.com>
2020-03-11 14:23:15 -0700

    Fixed all syntax errors

--- Lab3/operation.v
+++ Lab3/operation.v
@@ -1,7 +1,9 @@
+
 module operation(
 	input wire clk,
-	input wire [31:0] inst,
-	input wire [31:0] regdata1, regdata2, memdata	
+	input wire [31:0] inst0,
+	input wire [31:0] pc,
+	input wire [31:0] regdata1, regdata2, memdata,	
 	
 	output wire [3:0] regaddrIn, 
 	output wire [3:0] regaddrOut1, regaddrOut2, 
@@ -9,24 +11,37 @@ module operation(
 	output wire regwr, regrd1, regrd2,
 	output wire [31:0] memaddrIn, memaddrOut, memdataIn,
 	output wire memwr, memrd,
-	output wire bf
+	output wire bf,
+	output wire [31:0] branchimm
 	);
 	
+	reg [31:0] inst1, inst2, inst3, inst4;
+	
+	wire [31:0] result;
+	wire s;
+	wire [3:0] newcond;
+	
 	always @(posedge clk) begin
-		inst1 <= inst;
+		inst1 <= inst0;
 		inst2 <= inst1;
 		inst3 <= inst2;
 		inst4 <= inst3;
-		inst5 <= inst4;
+	end
+
+	reg [31:0] cpsr;
+	
+	// Altering CPSR
+	initial cpsr = 0;
+	always @(*) begin
+		if (s) cpsr = {newcond,cpsr[27:0]};
 	end
 	
-	stage2 two(clk, inst2, regrd1, regrd2, regaddrOut1, regaddrOut2);
+	stage1 one(clk, inst1, cpsr, regrd1, regrd2, regaddrOut1, regaddrOut2, bf, branchimm);
 	
-	stage3 three(clk, inst3, result);
+	stage2 two(clk, inst2, cpsr, regdata1, regdata2, memdataIn, result, s, newcond);
 	
-	stage4 four(clk, inst4, memwr, memrd, memaddrIn, memaddrOut, memdataIn);
+	stage3 three(clk, inst3, cpsr, regdata1, regdata2, memwr, memrd, memaddrIn, memaddrOut);//, memdataIn);
 	
-	stage5 five(clk, inst5, regwr, regaddrIn, regaddrOut2);
-
+	stage4 four(clk, inst4, cpsr, result, memdata, pc, regwr, regaddrIn, regdataIn);
 endmodule
 		
